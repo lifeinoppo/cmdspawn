a8ff44d65108fd279af5ea1e984aedaa20c0c904
srcspider <source.spider@gmail.com>
2015-06-04 23:01:07 +0300

    feat: helpers for working with valkyrja

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var process = require('child_process');
+var child_process = require('child_process');
 var Promise = require('lie');
 var type = require('typelib');
+var c = require('chalk');
 
 var defaults = {
 	tracking : false, // show start and end of command
@@ -21,7 +22,7 @@ var cmdspawn = function (conf) {
 		return new Promise(function (resolve, reject) {
 
 			if (conf.tracking) {
-				console.log(' start: ' + the_command);
+				console.log(c.blue('start') + ' ' + the_command);
 			}
 			else if (conf.verbose) {
 				console.log(' ' + the_command);
@@ -30,31 +31,32 @@ var cmdspawn = function (conf) {
 			var instructions = the_command.split(' ');
 			var command = instructions.shift();
 
-			var instance = process.spawn(command, instructions);
+			var instance = child_process.spawn(command, instructions);
 
 			instance.stdout.on('data', function (output) {
-				console.log(output.toString());
+				process.stdout.write(output.toString());
 			});
 
 			instance.stderr.on('data', function (err) {
-				console.log(err.toString());
+				process.stderr.write(err.toString());
 			});
 
 			instance.on('close', function (code) {
 				if (conf.tracking) {
-					console.log('   end: ' + the_command);
+					console.log('  ' + c.blue('end') + ' ' + the_command);
 				}
 
 				if (code == 0) {
 					resolve();
 				}
 				else { // code != 0
-					reject(new Error('Command ' + command + ' exited with code: ' + code))
+					reject(new Error('command ' + command + ' exited with code ' + code));
 				}
 			});
 
 			instance.on('error', function (err) {
-				console.log(' Failed calling ' + command + '; potentially unrecognized by system');
+				console.log('failed calling ' + command + '; potentially unrecognized by system');
+				reject(err);
 			})
 		});
 	};
@@ -67,6 +69,35 @@ var cmdspawn = function (conf) {
 	};
 
 	cmd.noop = function () {};
+
+	// promise error reporting
+	cmd.err = function (reason) {
+		if (reason) {
+			if (reason.stack) {
+				console.log('');
+				console.log(c.red.dim('error detected'));
+				console.log(reason.stack);
+			}
+			else { // no error stack
+				console.log('');
+				console.log(c.red.dim(reason))
+			}
+		}
+
+		return Promise.reject();
+	};
+
+	// no error messages for error
+	cmd.silent = function (reason) {
+		return Promise.reject();
+	};
+
+	// functor to be used in promise chaining
+	cmd.f = function (command) {
+		return function () {
+			return cmd(command);
+		};
+	};
 
 	return cmd;
 }
